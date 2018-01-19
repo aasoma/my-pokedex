7a8111ecb3180acec35c54615e8ac8fbbe7f43a1
aasoma <asoma2691@myhta.org>
2018-01-18 14:56:45 -1000

    finished the pokedex

--- js/app.js
+++ js/app.js
@@ -5,19 +5,20 @@
 // Find the div with the id of "name1" and replace the text n/a with "Tentacool"
 // Place your code below
 
-
+document.getElementById("name1").innerHTML= "Tentacool"
 
 // Pokemon 2
 // Find the div with the id of "type2" and replace the text n/a with "water"
 // Place your code below
 
-
+document.getElementById("type2").innerHTML= "water"
 
 
 // Pokemon 3
 // Find the div with the id of "pokeNumber3" and replace the text n/a with 116:
 // Place your code below
 
+document.getElementById("pokeNumber3").innerHTML= "116"
 
 
 
@@ -26,7 +27,7 @@
 // Find the div with the id of "bio4" and replace the text n/a with your personal description of this Pokemon.
 // Place your code below
 
-
+document.getElementById("bio4").innerHTML= "this dude hella lit"
 
 
 
@@ -34,7 +35,7 @@
 // Find the div with the id of "bio5" and replace the the font color to "red";
 // Place your code below
 
-
+document.getElementById("bio5").style.color= "red"
 
 
 
@@ -42,7 +43,7 @@
 // Find the div with the id of "name6" and replace the font size to "60px";
 // Place your code below
 
-
+document.getElementById("name6").style.fontSize="60px"
 
 
 
@@ -50,4 +51,4 @@
 // Find the div with the id of "container" and change the background color to "green"
 // Place your code below
 
-
+document.getElementById("container").style.backgroundColor= "green"
